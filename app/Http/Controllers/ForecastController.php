57adf84ed50236350ef1a6973b4b4e91b5e1aee1
Stefan Hoferichter <hoferich@gmx.de>
2025-08-08 10:50:06 +0200

    read  daily api

--- app/Http/Controllers/ForecastController.php
+++ app/Http/Controllers/ForecastController.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Http;
 class ForecastController extends Controller
 {
     
-    public function read_api()
+    public function read_hourly_api()
     {
         $url = "https://api.open-meteo.com/v1/forecast?latitude=52.5244&longitude=13.4105&hourly=temperature_2m,precipitation,cloud_cover&forecast_days=1";
         
@@ -43,6 +43,20 @@ class ForecastController extends Controller
         
         $labels = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00','18:00', '19:00', '20:00', '21:00', '22:00', '23:00'];
         return view('forecast_list', ['precipitation' => $precipitation, 'temperatures' => $temperatures, 'cloudCovers' => $cloudCovers, 'labels' => $labels]);
+    }
+    
+    
+    public function read_daily_api()
+    {
+        $url = "https://api.open-meteo.com/v1/forecast?latitude=52.5244&longitude=13.4105&daily=temperature_2m_max,temperature_2m_min,sunshine_duration,rain_sum&timezone=Europe%2FBerlin&forecast_days=1";
+        
+        $response = $this->callApi($url);
+
+        echo  $response['daily']['temperature_2m_min'][0]. "<br>";
+        echo  $response['daily']['temperature_2m_max'][0]. "<br>";
+        echo  $response['daily']['sunshine_duration'][0]. "<br>";
+        echo  $response['daily']['rain_sum'][0]. "<br>";
+        
     }
     
     public function callApi($url)
