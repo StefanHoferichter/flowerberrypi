0302f8babd3aaecdff4d2e0047a0cc200d97ee64
Stefan Hoferichter <hoferich@gmx.de>
2025-09-13 19:26:46 +0200

    fine tunimg submenu

--- resources/views/include_setup_menu.blade.php
+++ resources/views/include_setup_menu.blade.php
@@ -1,7 +1,7 @@
 <div class="nav_submenu">
           <div class="nav_setup_zones"><a class="nav_item" href="/setup_zones">Zones</a></div>
           <div class="nav_setup_sensors"><a class="nav_item" href="/setup_sensors">Sensors</a></div>
-          <div class="nav_setup_remote_sockets"><a class="nav_item"  href="/setup_remote_sockets">Remote Sockets</a></div>
-          <div class="nav_setup_percentage_conversion"><a class="nav_item" href="/setup_percentage_conversions">Percentage Conversion</a></div>
+          <div class="nav_setup_remote_sockets"><a class="nav_item"  href="/setup_remote_sockets">Sockets</a></div>
+          <div class="nav_setup_percentage_conversion"><a class="nav_item" href="/setup_percentage_conversions">Percentages</a></div>
           <div class="nav_setup_thresholds"><a class="nav_item"  href="/setup_thresholds">Thresholds</a></div>
 </div>  
