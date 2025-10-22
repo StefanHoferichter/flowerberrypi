991aa5d60a3cd80e0b2ab08bd1e948686d0d8224
Stefan Hoferichter <hoferich@gmx.de>
2025-10-22 17:14:48 +0200

    cleanup

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Jobs\ProcessData;
-use App\Models\SensorJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
