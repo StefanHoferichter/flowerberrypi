d5ff914af486ead38712cbf01cd5a23a26c5e179
Stefan Hoferichter <hoferich@gmx.de>
2025-12-03 21:53:36 +0100

    addes shelly

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
     Welcome to FlowerBerryPi, your smart personal watering solution for flowers and plants.<br><br>
     
     FlowerBerryPi uses soil moisture sensors to monitor the soil’s moisture level. Indoor temperature is measured with a sensor, while outdoor temperature is obtained from an online weather service. The water level in the tanks is monitored using ultrasonic distance sensors.<br>
-    Plants are watered either via a Gardena Vacation Watering Set controlled by Brennenstuhl remote sockets, or via 5V water pumps operated through a relay.<br>
+    Plants are watered either via a Gardena Vacation Watering Set controlled by Brennenstuhl remote sockets or Shelly WiFi Sockets, or via 5V water pumps operated through a relay.<br>
     FlowerBerryPi combines hardware (Raspberry Pi, sensors, PCBs, cables) with software (a web UI built on Laravel).<br><br>
     
     FlowerBerryPi requires an initial configuration. First, zones need to be defined. A zone consists of a water tank with an ultrasonic level sensor and a pump. Optionally, soil moisture sensors can be added.<br>
